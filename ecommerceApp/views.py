9ce90537a87908ac0dc2c5d7402d1174d66f1d96
gojamont <94007991+gojamont@users.noreply.github.com>
2025-09-24 12:33:27 +0200

    managed django project with links, html only

---
+++ ecommerceApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# index page
+def index(request):
+    return render(request, "home.html");
+
+def about(request):
+    return render(request, "AboutPage.html");
+
+def login(request):
+    return render(request, "loginPage.html");
+
+def promoCodePage(request):
+    return render(request, "promoCodePage.html");
+
+def register(request):
+    return render(request, "registerPage.html");
+
+def catalogPage(request):
+    return render(request, "CatalogPage.html");
+
