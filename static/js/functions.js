39b13b5f62ee7791a5406f160764977df191ff46
anpaz24 <anpaz24@student.sdu.dk>
2025-10-05 18:47:16 +0200

    added mobile css for topbar and sidebar

--- static/js/functions.js
+++ static/js/functions.js
@@ -25,3 +25,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    menuIcon = document.getElementById("menu");
+    overlay = document.getElementById("overlay");
+    cross = document.getElementById("cross");
+
+    menuIcon.addEventListener("click", showMenu);
+    overlay.addEventListener("click", hideMenu);
+    cross.addEventListener("click", hideMenu);
+});
+
+function showMenu () {
+    let sideMenu = document.getElementById("side-menu");
+    let overlay = document.getElementById("overlay");
+
+    sideMenu.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+function hideMenu () {
+    let sideMenu = document.getElementById("side-menu");
+    let overlay = document.getElementById("overlay");
+
+    sideMenu.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
