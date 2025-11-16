c33c9d81fb4e119b5b4cf2656dca2dca1d8c1c06
gojamont <94007991+gojamont@users.noreply.github.com>
2025-11-16 12:32:14 +0100

    added login logic

--- ecommerceApp/views.py
+++ ecommerceApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib.auth import authenticate, login
+import json
 
 # Create your views here.
 
@@ -10,12 +12,34 @@ def index(request):
 def about(request):
     return render(request, "AboutPage.html");
 
-def login(request):
-    return render(request, "loginPage.html");
+# function for authenticating the user
+def login_user(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+            
+            if not username or not password:
+                return render(request, "loginPage.html", {'error':'Username or password is required'})
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                return render(request, "loginPage.html", {'error':'Invalid credentials'})
+        except json.JSONDecodeError:
+                return render(request, "loginPage.html", {'error':'Invalid request'})
+        except Exception as error:
+                return render(request, "loginPage.html", {'error':'Error occurred while trying to login a user, please try again'})
+    return render(request, "loginPage.html")
 
 def promoCodePage(request):
     return render(request, "promoCodePage.html");
 
+# function for registering the user
 def register(request):
     return render(request, "registerPage.html");
 
